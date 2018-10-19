9e8aaa1efb4f0a1fb00997c4e059f1e41a010adc
Brigette Eckert <brigette.eckert@live.com>
2018-10-19 13:27:04 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1> Free Code Camp PDX is excited to join the JAMSTACK Hackathon! </h1>
+  </Layout>
+)
+
+export default IndexPage
